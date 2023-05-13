225d10cdd56fa90ec118ec5b2c3e7509dfcfe091
Maula <omjipanggg@yahoo.com>
2023-05-13 12:28:38 +0700

    Amora an amita

--- resources/views/components/modals/control.blade.php
+++ resources/views/components/modals/control.blade.php
@@ -1,1 +1,16 @@
-control.blade.php
+<div class="modal fade" id="modalControl" tabindex="-1" aria-labelledby="modalControlLabel" aria-hidden="true">
+<div class="modal-dialog modal-xl modal-fullscreen-lg-down modal-dialog-centered modal-dialog-scrollable">
+<div class="modal-content">
+<div class="modal-header">
+<h5 class="modal-title">Table</h5>
+<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+</div>
+<div class="modal-body" id="modalControlBody">
+<div id="modalControlPlaceholders">Fetching...</div>
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-color" id="saveTrigger">Submit<i class="fas fa-floppy-disk ms-2"></i></button>
+</div>
+</div>
+</div>
+</div>
